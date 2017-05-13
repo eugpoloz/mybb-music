2afdafc24f676fcf4155c85a5b7df8024057c194
Evgenia Polozova <eugpoloz@gmail.com>
2017-05-13 16:44:56 +0300

    fix prostopleer track width

--- mybb-music.js
+++ mybb-music.js
@@ -48,7 +48,7 @@ var letsMakeSomeMusic = function() {
     let playerParams = {
       type: 'track',
       id: url[2],
-      w: '100%',
+      w: 578,
       h: 43
     };
     let {type, id, w, h} = playerParams;
@@ -116,85 +116,64 @@ var letsMakeSomeMusic = function() {
   }
 
   let createPopup = () => {
-    let audioPopup = document.createElement('div');
-    audioPopup.className = 'container audio-popup';
-    audioPopup.id = 'audio-area';
-    audioPopup.style.display = 'none';
-    audioPopup.innerHTML = `
+    let popup = document.createElement('div');
+    popup.className = 'container audio-popup';
+    popup.id = 'audio-area';
+    popup.style.display = 'none';
+    popup.innerHTML = `
       <label for="audio-link-input" class="audio-popup__label">
-        <strong>Вставьте ссылку на трек с Яндекс.Музыки, Pleer.net или Zippyshare:</strong>
+        <strong>Вставьте ссылку на трек с Яндекс.Музыки, Простоплеера или zippyshare.com:</strong>
         <input class="audio-popup__input" type="text" id="audio-link-input" name="audio-link-input" placeholder="http://" />
       </label>
-      <a href="#" class="audio-popup__btn js-audio-link-insert">Вставить</a> <a href="#" class="audio-popup__btn js-audio-link-close">Закрыть</a>
+      <a href="#" class="audio-popup__btn js-audio-link-insert">Вставить</a>&nbsp;&nbsp;&nbsp;<a href="#" class="audio-popup__btn js-audio-link-close">Закрыть</a>
     `;
 
-    return audioPopup;
+    return popup;
   }
+
+  // insert btn
+  const codeBtn = document.getElementById('button-code');
+  insertNode(createBtn(), codeBtn);
+
+  // insert popup
+  const colorDiv = document.querySelector('#post .fs-box.hashelp #color-area');
+  insertNode(createPopup(), colorDiv);
+
+  // cash audioPopup and audioInput references for helper functions to use
+  const audioPopup = document.getElementById('audio-area');
 
   // function on btn
   function addMusic(e) {
-    if (e.ctrlKey || e.altKey) return bbcode('[audio]', '[/audio]');
-
-    let audioPopup = document.getElementById('audio-area');
-
     let isVisible = audioPopup.style.display === 'block';
+
+    if (e.ctrlKey || e.altKey) {
+      if (isVisible) audioPopup.style.display = 'none'; // hide popup if it's shown
+      return bbcode('[audio]', '[/audio]');
+    }
+
     audioPopup.style.display = isVisible ? 'none' : 'block';
     audioPopup.style.zIndex = isVisible ? -1 : 10000;
   }
 
-  const codeBtn = document.getElementById('button-code');
-  insertNode(createBtn(), codeBtn);
+  function popupBtnActions(e) {
+    e.preventDefault();
+    audioPopup.style.display = 'none';
+    
+    let input = document.getElementById('audio-link-input');
+    
+    if ( e.target === document.querySelector('.js-audio-link-insert')) {
+      input.value.trim() === '' ? bbcode('[audio]', '[/audio]') : insert(`[audio]${input.value}[/audio]`);
+    }
 
-  const colorDiv = document.querySelector('#post .fs-box.hashelp #color-area');
-  insertNode(createPopup(), colorDiv);
+    return input.value = '';
+  }
 
   const audioBtn = document.getElementById('button-audio');
   audioBtn.addEventListener('click', addMusic, true);
-}();
 
-// КОД ВЕРСИИ 2.0
-// $(function() {
-//   $('.post-content').find('p').each(function() {
-//     if ( (/\[audio\](?:[^\]])+\[\/audio\]/gi).test($(this).html()) == true ) {
-//       if ( (/\[audio\]https\:\/\/music\.yandex\.ru(?:[^\]])+\[\/audio\]/gi).test($(this).html()) == true ) {
-//         var yandexTrack = /\[audio\]https\:\/\/music\.yandex\.ru\/album\/((?:[\d\]])+)\/track\/((?:[\d\]])+)\[\/audio\]/gi,
-//           yandexAlbum = /\[audio\]https\:\/\/music\.yandex\.ru\/album\/((?:[\d\]])+)\[\/audio\]/gi,
-//           yandexList = /\[audio\]https\:\/\/music\.yandex\.ru\/users\/((?:[^\]])+)\/playlists\/((?:[\d\]])+)\[\/audio\]/gi;
-//         if ( yandexTrack.test($(this).html()) == true ) {
-//           $(this).html( $(this).html().replace(yandexTrack, '<iframe frameborder=\"0\" style=\"border:none;width:100%;height:70px;\" width=\"100%\" height=\"70\" src=\"https:\/\/music.yandex.ru\/iframe\/#track\/$2\/$1\/hide\/cover\/\"><\/iframe>')); //Вставка трека с Яндекса
-//         } else if ( yandexAlbum.test($(this).html()) == true ) {
-//           $(this).html( $(this).html().replace(yandexAlbum, '<iframe frameborder="0" style="border:none;width:100%;height:500;" width="100%" height="500" src="https://music.yandex.ru/iframe/#album/$1/hide/cover/"></iframe>')); //Вставка альбома с Яндекса
-//         } else if ( yandexList.test($(this).html()) == true ) {
-//           $(this).html( $(this).html().replace(yandexList, '<iframe frameborder="0" style="border:none;width:100%;height:500;" width="100%" height="500" src="https://music.yandex.ru/iframe/#playlist/$1/$2/show/description/hide/cover/"></iframe>')); //Вставка плейлиста с Яндекса
-//         }
-//       } else if ( (/\[audio\]http\:\/\/www((?:[^\]])+)\.zippyshare\.com(?:[^\]])+\[\/audio\]/gi).test($(this).html()) == true ) {
-//         $(this).html($(this).html().replace(/\[audio\]http\:\/\/www((?:[^\]])+)\.zippyshare\.com\/v\/((?:[^\]])+)\/file\.html\[\/audio\]/gi, '<embed type="application/x-shockwave-flash" src="http://api.zippyshare.com/api/mediaplayer/mediaplayer.swf" flashvars="height=20&amp;width=400&amp;file=http://www$1.zippyshare.com/downloadMusic%3Fkey%3D$2%26&amp;volume=80&amp;autostart=false&amp;frontcolor=0x000000&amp;backcolor=0xffffff&amp;lightcolor=0x000000&amp;type=flv" quality="high" menu="false" wmode="transparent" allowscriptaccess="always" height="20" width="400" name="mp3player" border="0" style="margin-bottom: 6px;"/>')); // Вставка трека с zippyshare
-//       } else if ( (/\[audio\]http\:\/\/(?:prostopleer|pleer)\.(?:com|net)(?:[^\]])+\[\/audio\]/gi).test($(this).html()) == true ) {
-//         var pleerTrack = /\[audio\]http:\/\/(?:prostopleer|pleer)\.(?:com|net)\/tracks\/((?:[^\]])+)\[\/audio\]/gi,
-//           pleerList = /\[audio\]http:\/\/(?:prostopleer|pleer)\.(?:com|net)\/list((?:[^\]])+)\[\/audio\]/gi;
-//           if ( pleerTrack.test($(this).html()) == true ) {
-//             $(this).html($(this).html().replace( pleerTrack, '<object width="578" height="43"><param name="movie" value="http://embedpleer.net/normal/track?id=$1&t=grey"></param><embed src="http://embedpleer.net/normal/track?id=$1&t=grey" type="application/x-shockwave-flash" width="578" height="43"></embed></object>')); // Вставка трека с (prosto)pleer.com
-//           } else if ( pleerList.test($(this).html()) == true ) {
-//             $(this).html($(this).html().replace(pleerList, '<object width="550" height="300"><param name="movie" value="http://embedpleer.net/list?id=$1"></param><embed src="http://embedpleer.net/list?id=$1" type="application/x-shockwave-flash" width="550" height="300" style="margin-bottom:-90px;"></embed></object>')); // Вставка плейлиста с (prosto)pleer.com
-//           }
-//       }
-//     }
-//   });
-// });
-// // Кнопка
-// $('td#button-code').after('<td id="button-audio" style="background-image:url(http://s5.uploads.ru/4QUIa.gif);" title="Музыка в посте"><img src="/i/blank.gif"></td>');
-// // Функция на кнопке
-// $('td#button-audio').click(function (event) {
-//   if (event.ctrlKey || event.altKey) {
-//     bbcode('[audio]', '[/audio]');
-//   } else {
-//     var audioMusicURL = prompt('Вставьте ссылку на трек, альбом или плейлист с Яндекс.Музыки, трек или плейлист с pleer.net или трек с zippyshare.com', 'http://');
-//     if (audioMusicURL === null) {
-//       return;
-//     } else if (!audioMusicURL || audioMusicURL === 'http://') {
-//       alert('Вы не вставили ссылку!');
-//     } else {
-//       insert('[audio]' + audioMusicURL + '[/audio]');
-//     }
-//   }
-// });
+  const insertCodeBtn = document.querySelector('.js-audio-link-insert');
+  const closePopupBtn = document.querySelector('.js-audio-link-close');
+
+  insertCodeBtn.addEventListener('click', popupBtnActions, true);
+  closePopupBtn.addEventListener('click', popupBtnActions, true);
+}();
